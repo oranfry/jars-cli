d0d2608dbb963f5530e7e19abdaaa04f2f57cf27
Oran Fry <oran@emerg.co>
2022-03-20 14:05:52 +1300

    Fixes

--- lib.php
+++ lib.php
@@ -1,5 +1,7 @@
 <?php
 
+// const JARS_PERSIST_PER_IMPORT = 1;
+
 function read_password()
 {
     $f = popen("/bin/bash -c 'read -s; echo \$REPLY'", "r");
@@ -66,8 +68,72 @@ function import_presave($data, $jars, $timestamp)
 {
     global $linetype;
 
+    id_map_r($data, $jars);
+
     foreach ($data as $line) {
         $line->type = $linetype;
     }
 }
 
+function id_map_r($data, $jars)
+{
+    global $id_map;
+
+    foreach ($data as $line) {
+        if (@$line->id) {
+            $orig = $line->id;
+            $line->id = @$id_map[$line->id] ?? $line->id;
+
+            // if ($orig !== $line->id) {
+            //     echo "Mapped $orig to {$line->id}\n";
+            // }
+        }
+
+        foreach (get_object_vars($line) as $key => $value) {
+            if (preg_match('/_id$/', $key)) {
+                $orig = $line->$key;
+                $line->$key = @$id_map[$line->$key] ?? $line->$key;
+
+                // if ($orig !== $line->$key) {
+                //     echo "Mapped $orig to {$line->$key}\n";
+                // }
+            }
+        }
+
+        if (@$line->_adopt) {
+            foreach ($line->_adopt as $set => &$children) {
+                foreach ($children as $i => $child_id) {
+                    $orig = $children[$i];
+                    $children[$i] = @$id_map[$child_id] ?? $child_id;
+
+                    // if ($orig !== $children[$i]) {
+                    //     echo "Mapped $orig to {$children[$i]}\n";
+                    // }
+                }
+            }
+        }
+
+        if (@$line->_disown) {
+            foreach ($line->_disown as $set => &$children) {
+                if ($set == 'teachers') {
+                    continue;
+                }
+
+                foreach ($children as $i => $child_id) {
+                    $orig = $children[$i];
+                    $children[$i] = @$id_map[$child_id] ?? $child_id;
+
+                    // if ($orig !== $children[$i]) {
+                    //     echo "Mapped $orig to {$children[$i]}\n";
+                    // }
+                }
+            }
+        }
+
+        foreach (array_keys(get_object_vars($line)) as $key) {
+            if (is_array($line->$key)) {
+                id_map_r($line->$key, $jars);
+            }
+        }
+    }
+}
