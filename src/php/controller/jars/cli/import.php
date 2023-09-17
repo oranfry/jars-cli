79bad05ca21cbfe3f7f39326f25f309facb7b572
Oran Fry <oran@emerg.co>
2023-09-17 14:13:09 +1200

    Import
    Remove pre and post save callbacks, relying on feedback instead. Specify differential import as this is no longer the default.

--- src/php/controller/jars/cli/import.php
+++ src/php/controller/jars/cli/import.php
@@ -17,33 +17,17 @@ echo "Importing\n\n";
 while ($f = fgets(STDIN)) {
     [$hash, $date, $time, $json] = explode(' ', $f, 4);
 
-    $timestamp = "{$date} {$time}";
+    $timestamp = $date . ' ' . $time;
     $data = json_decode($json);
-
-    if (function_exists('import_presave')) {
-        $result = import_presave($data, $jars, $timestamp);
-
-        if (is_array($result)) {
-            extract($result);
-        }
-    }
 
     echo $timestamp . "\n";
 
-    $data = $jars->import($timestamp, array_values($data), false, 0);
+    $data = $jars->import($timestamp, array_values($data), false, 0, true);
 
     echo "\n";
 
     if ($data === false) {
         error_response("Import error");
-    }
-
-    if (function_exists('import_postsave')) {
-        $result = import_postsave($data, $jars, $timestamp);
-
-        if (is_array($result)) {
-            extract($result);
-        }
     }
 }
 
