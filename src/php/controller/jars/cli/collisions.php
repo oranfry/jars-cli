9b9b0a238225f0cd766e506081ce4a3bd11f1681
Oran Fry <oran@emerg.co>
2024-06-16 15:21:01 +1200

    Collisions
    For huge collision searches, we need to make use of the hard drive plus the memory. This attempts to achieve that.

--- src/php/controller/jars/cli/collisions.php
+++ src/php/controller/jars/cli/collisions.php
@@ -1,17 +1,45 @@
 <?php
 
+$pid = getmypid() ?: 'U' . rand(100000, 999999);
+$dir = sys_get_temp_dir() . '/jars-collisions-' . $pid;
+
+echo "Using data directory: $dir\n";
+
 $lookup = [];
 $collisions = [];
 
 for ($i = 1; $i < MAX; $i++) {
     $id = $jars->n2h($i);
+    $file = $dir . '/' . substr($id, 0, 1) . '/' . substr($id, 1, 1) . '/' . substr($id, 2, 1) . '/' . substr($id, 3, 1) . '/' . substr($id, 4);
 
-    if (isset($lookup[$id])) {
+    if (file_exists($file)) {
         $collisions[] = $i;
     }
 
-    $lookup[$id] = $i;
+    @mkdir(dirname($file), 0777, true);
+
+    touch($file);
 }
+
+$rrmdir = function ($dir) use (&$rrmdir) {
+    if (!is_dir($dir)) {
+        return;
+    }
+
+    foreach (scandir($dir) as $object) {
+        if ($object !== '.' && $object !== '..') {
+            if (is_dir($dir . '/' . $object) && !is_link($dir . '/' . $object)) {
+                $rrmdir($dir . '/' . $object);
+            } else {
+                unlink($dir. '/' . $object);
+            }
+        }
+    }
+
+    rmdir($dir);
+};
+
+$rrmdir($dir);
 
 return [
     'collisions' => $collisions,
