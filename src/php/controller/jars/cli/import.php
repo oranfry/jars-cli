74945a776568c228a63bc99c704aa2c6d9488752
Oran Fry <oran@emerg.co.nz>
2026-07-20 09:00:28 +1200

    Import
    Give updates on memory usage as we go.

--- src/php/controller/jars/cli/import.php
+++ src/php/controller/jars/cli/import.php
@@ -3,6 +3,21 @@
 use OranFry\Jars\CLI\ImportListener;
 use OranFry\Jars\Contract\Exception;
 
+if (!function_exists('numberToSiSuffix')) {
+    function numberToSiSuffix($number) {
+        $suffixes = ['', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
+        $power = floor(log(abs($number), 1000));
+        
+        // Handle numbers less than 1 (no suffix)
+        if ($power < 1 || !isset($suffixes[$power])) {
+            return number_format($number);
+        }
+        
+        $num = $number / pow(1000, $power);
+        return number_format($num, 1) . $suffixes[$power];
+    }
+}
+
 if (FEEDBACK_FIFO) {
     echo "Opening feedback fifo for WRITE...\n";
 
@@ -25,8 +40,8 @@ while ($f = fgets(STDIN)) {
     echo $timestamp . "\n";
 
     $jars->import($timestamp, array_values(json_decode($json)), null, false, 0, true);
-
     echo "\n";
+    echo 'Memory usage: ' . numberToSiSuffix(memory_get_usage()) . "\n";
 }
 
 $jars->unlockPrimary($pin);
